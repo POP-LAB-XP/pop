f582ce4ff144cac8e7aa64c4421770e7a8aaf74c
Thiago Araujo <thd.araujo@gmail.com>
2015-03-27 19:04:16 -0300

    Cadastro de usuario e Login. Falta ajustar a interface.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   #get 'static_pages/welcome'
   root 'static_pages#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
