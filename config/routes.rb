adc724e53dc63721a54ed47dcd98e05a6f25f55a
Rafael Gregorut <rafaelgregorut@Rafaels-MacBook-Pro.local>
2015-03-27 19:03:41 -0300

    criacao da interface de login com bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'interface/index'
+
   #get 'static_pages/welcome'
   root 'static_pages#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
