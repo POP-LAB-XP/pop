0acff62584272e6f557ceba0977de728a05dd4a2
Thiago Araujo <thd.araujo@gmail.com>
2015-04-08 13:12:38 -0300

    faltava dar override na rota do registration e mover as views de pasta

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
   #devise_for :users, :controllers => { :registrations => "registrations" }
 
   #get 'static_pages/welcome'
