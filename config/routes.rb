019a00ca2717345041154980055b943378bcac80
Thiago Araujo <thd.araujo@gmail.com>
2015-04-08 16:38:52 -0300

    merge da tela de login

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   #devise_for :users
   
   devise_for :users, controllers: { registrations: "users/registrations" }
 
   #devise_for :users, :controllers => { :registrations => "registrations" }
-=======
-  get 'interface/index'
->>>>>>> login
+  #get 'interface/index'
+
+  get 'static/index'
 
   #get 'static_pages/welcome'
-  root 'static_pages#welcome'
+  root 'static#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
