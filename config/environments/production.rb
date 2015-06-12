0c627ec220ee4a2bb2d6ab4c4aae10bb7fc04787
Thiago Araujo <worf.mogh@gmail.com>
2015-06-12 19:02:12 -0300

    Ajuste na config de email do production.rb environment [@thdaraujo, @mayara-melo]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'pop.lab.xp@gmail.com',
+    password:             'poplabxp1234',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
+  
 end
