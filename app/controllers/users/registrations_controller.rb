5a268a7d49304eb586f5bf45ce2966438ab29f37
andre-yai <yai.andre@gmail.com>
2015-06-12 15:15:19 -0300

    Realizamos as ultimas modificacoes na interface.
    Retiramos o navbar das paginas de login, cadastro e boas-vindas e trocamos o texto do botao de 'Nova Proposta' para 'Criar' [@felipetuliopc, @mayara-melo]

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,17 +3,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-   super do |resource|
+    super do |resource|
         subPrefeitura = SubPrefeitura.find_by_codigo(params[:codigo])
         if not subPrefeitura.limite_usuario_atingido
           resource.sub_prefeitura = subPrefeitura
           resource.save
-	  p resource.errors
+          p resource.errors
         else
           flash[:notice] = "Limite de usuários atingido!"
         end
     end
- end
+  end
 
   protected
 
